2527f4b5e99f4d6667084565d7a185a22b97bf96
PiYujie <pyjpi@foxmail.com>
2018-05-20 09:34:51 +0800

    1

--- index.js
+++ index.js
@@ -2,30 +2,32 @@ import Vue from "vue";
 // 路由
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+
 // 状态管理
 import Vuex from 'vuex'
 Vue.use(Vuex)
 // 全局样式
 import "./template/base.css";
 //选项卡组件
-import home from "./components/tab/app-home.vue";
-import mine from "./components/tab/app-mine.vue";
-import signin from "./components/tab/app-signin.vue";
-import activity from "./components/tab/app-activity.vue";
+import home from "./components/tab/app_home.vue";
+import mine from "./components/tab/app_mine.vue";
+import signin from "./components/tab/app_signin.vue";
+import activity from "./components/tab/app_activity.vue";
+import actType from "./components/tab/app_activityType.vue";
 //容器组件
-import detail from "./components/contain/app-detail.vue";
-import index from "./components/contain/app-index.vue";
-import login from "./components/contain/app-login.vue";
-import setting from "./components/contain/app-setting.vue";
-import own from "./components/contain/app-own.vue";
-import upmess from "./components/contain/app-up-mess.vue";
-import inmess from "./components/contain/app-in-mess.vue";
-import register from "./components/contain/app-register.vue";
-import safe from "./components/contain/app-safe.vue";
+import detail from "./components/contain/app_detail.vue";
+import index from "./components/contain/app_index.vue";
+import login from "./components/contain/app_login.vue";
+import setting from "./components/contain/app_setting.vue";
+import own from "./components/contain/app_own.vue";
+import upmess from "./components/contain/app_upMess.vue";
+import inmess from "./components/contain/app_inMess.vue";
+import register from "./components/contain/app_register.vue";
+import safe from "./components/contain/app_safe.vue";
 //公共组件
-import footer from "./components/app-footer.vue";
-import header from "./components/app-header.vue";
-import search from "./components/app-search.vue";
+import footer from "./components/app_footer.vue";
+import header from "./components/app_header.vue";
+import search from "./components/app_search.vue";
 // 配置路由
 var router = new VueRouter({
 	routes:[
@@ -63,6 +65,10 @@ var router = new VueRouter({
 				},{
 					path: '/index',
 					redirect: '/index/home'
+				},{
+					//活动
+					path: "actType/:id",
+                    component: actType
 				}
 			]
 		},
@@ -94,7 +100,8 @@ var router = new VueRouter({
 		},
 		//详情
 		{
-			path: '/detail',
+			name:'detail',
+			path: '/detail/:id',
 			component: detail
 		},
 		//注册
